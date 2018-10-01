e4ccd63f58828ea9e1c9abc182a9cfb2bdad34cb
Adam <michalak.adam@protonmail.com>
2018-10-01 11:53:25 +0200

    feat: add enum that contains all possible coins served by ticket machine

---
+++ src/main/java/com/michalak/adam/helpers/Coin.java
@@ -0,0 +1,14 @@
+package com.michalak.adam.helpers;
+
+public enum Coin {
+    FIVE(5.0), TWO(2.0), ONE(1.0), POINTFIFTY(0.50), POINTTWENTY(0.20), POINTTEN(0.10);
+
+    private double value;
+
+    Coin(double value){
+        this.value = value;
+    }
+    public String toString(){
+        return value + value > 0.9 ? "zł" : "gr";
+    }
+}
