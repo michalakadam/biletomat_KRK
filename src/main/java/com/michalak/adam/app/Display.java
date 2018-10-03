f52a13d4cfcc5bbe181549d516dbe2cec1950ac4
Adam <michalak.adam@protonmail.com>
2018-10-03 14:23:43 +0200

    feat: update Display to work properly with ShoppingCart object

--- src/main/java/com/michalak/adam/app/Display.java
+++ src/main/java/com/michalak/adam/app/Display.java
@@ -13,12 +13,13 @@ import java.util.ArrayList;
 
 public class Display {
     protected Printer printer;
-    protected ArrayList<Ticket> ticketsBought;
+    private ShoppingCart shoppingCart;
 
     public Display() {
         this.printer = new Printer();
-        this.ticketsBought = new ArrayList<Ticket>();
+        this.shoppingCart = new ShoppingCart();
     }
+
     //method that is going to display information for a client throughout the process
     public void screen(Scanner keyboard) {
         //first let's check if tickets can be printed on paper
@@ -38,15 +39,17 @@ public class Display {
             decision = UserInputProvider.getInputFromUser(keyboard, "Czy chcesz kupić dodatkowe bilety? (1. tak/2. nie)", 1, 2);
             if (decision == 1)
                 initialScreen(keyboard);
-        } while(decision == 1);
+        } while (decision == 1);
         screen(keyboard); //
     }
+
     public void checkForPaper() throws NoPaperException {
-        if(!printer.checkPaper()) { //information displayed until technician will refill the paper
+        if (!printer.checkPaper()) { //information displayed until technician will refill the paper
             throw new NoPaperException("Brak papieru. Przepraszamy za utrudnienia.");
         }
     }
-    private void initialScreen(Scanner keyboard){
+
+    private void initialScreen(Scanner keyboard) {
         //this is obviously going to be a touch screen in real ticket machine but I will use numbers to make it work on computer
         int decision;
         decision = UserInputProvider.getInputFromUser(keyboard, "1. Bilety czasowe I strefa miasto Kraków\n2. Bilety czasowe I + II aglomeracja", 1, 2);
@@ -55,78 +58,68 @@ public class Display {
         else if (decision == 2)
             biletyAglomeracja(keyboard);
     }
-    private void biletyMiasto(Scanner keyboard){
+
+    private void biletyMiasto(Scanner keyboard) {
         int decision;
         int quantity;
-        System.out.println("1. \n"+Ticket.TWENTYREDUCEDZONEI + "2. \n"+Ticket.TWENTYZONEI);
-        System.out.println("3. \n"+Ticket.FOURTYREDUCEDZONEI + "4. \n"+Ticket.FOURTYZONEI);
-        System.out.println("5. \n"+Ticket.SIXTYREDUCEDZONEI + "6. \n"+Ticket.SIXTYZONEI);
-        System.out.println("7. \n"+Ticket.NINETYREDUCEDZONEI + "8. \n"+Ticket.NINETYZONEI);
+        System.out.println("1. \n" + Ticket.TWENTYREDUCEDZONEI + "2. \n" + Ticket.TWENTYZONEI);
+        System.out.println("3. \n" + Ticket.FOURTYREDUCEDZONEI + "4. \n" + Ticket.FOURTYZONEI);
+        System.out.println("5. \n" + Ticket.SIXTYREDUCEDZONEI + "6. \n" + Ticket.SIXTYZONEI);
+        System.out.println("7. \n" + Ticket.NINETYREDUCEDZONEI + "8. \n" + Ticket.NINETYZONEI);
         decision = UserInputProvider.getInputFromUser(keyboard, "Wybierz bilet: ", 1, 8);
         quantity = pickQuantity(keyboard);
-        for (int i = 1; i <= quantity; i++){
+        for (int i = 1; i <= quantity; i++) {
             if (decision == 1)
-                addTicketToShoppingList(Ticket.TWENTYREDUCEDZONEI);
+                shoppingCart.addTicketToCart(Ticket.TWENTYREDUCEDZONEI);
             else if (decision == 2)
-                addTicketToShoppingList(Ticket.TWENTYZONEI);
+                shoppingCart.addTicketToCart(Ticket.TWENTYZONEI);
             else if (decision == 3)
-                addTicketToShoppingList(Ticket.FOURTYREDUCEDZONEI);
+                shoppingCart.addTicketToCart(Ticket.FOURTYREDUCEDZONEI);
             else if (decision == 4)
-                addTicketToShoppingList(Ticket.FOURTYZONEI);
+                shoppingCart.addTicketToCart(Ticket.FOURTYZONEI);
             else if (decision == 5)
-                addTicketToShoppingList(Ticket.SIXTYREDUCEDZONEI);
+                shoppingCart.addTicketToCart(Ticket.SIXTYREDUCEDZONEI);
             else if (decision == 6)
-                addTicketToShoppingList(Ticket.SIXTYZONEI);
+                shoppingCart.addTicketToCart(Ticket.SIXTYZONEI);
             else if (decision == 7)
-                addTicketToShoppingList(Ticket.NINETYREDUCEDZONEI);
+                shoppingCart.addTicketToCart(Ticket.NINETYREDUCEDZONEI);
             else if (decision == 8)
-                addTicketToShoppingList(Ticket.NINETYZONEI);
+                shoppingCart.addTicketToCart(Ticket.NINETYZONEI);
         }
     }
-    private void biletyAglomeracja(Scanner keyboard){
+
+    private void biletyAglomeracja(Scanner keyboard) {
         int decision;
         int quantity;
-        System.out.println("1. \n"+Ticket.RIDEREDUCEDZONEII + "2. \n"+Ticket.RIDEZONEII);
-        System.out.println("3. \n"+Ticket.SIXTYREDUCEDZONEII + "4. \n"+Ticket.SIXTYZONEII);
-        System.out.println("5. \n"+Ticket.NINETYREDUCEDZONEII + "6. \n"+Ticket.NINETYZONEII);
+        System.out.println("1. \n" + Ticket.RIDEREDUCEDZONEII + "2. \n" + Ticket.RIDEZONEII);
+        System.out.println("3. \n" + Ticket.SIXTYREDUCEDZONEII + "4. \n" + Ticket.SIXTYZONEII);
+        System.out.println("5. \n" + Ticket.NINETYREDUCEDZONEII + "6. \n" + Ticket.NINETYZONEII);
         decision = UserInputProvider.getInputFromUser(keyboard, "Wybierz bilet: ", 1, 6);
         quantity = pickQuantity(keyboard);
-        for (int i = 1; i <= quantity; i++){
+        for (int i = 1; i <= quantity; i++) {
             if (decision == 1)
-                addTicketToShoppingList(Ticket.RIDEREDUCEDZONEII);
+                shoppingCart.addTicketToCart(Ticket.RIDEREDUCEDZONEII);
             else if (decision == 2)
-                addTicketToShoppingList(Ticket.RIDEZONEII);
+                shoppingCart.addTicketToCart(Ticket.RIDEZONEII);
             else if (decision == 3)
-                addTicketToShoppingList(Ticket.SIXTYREDUCEDZONEII);
+                shoppingCart.addTicketToCart(Ticket.SIXTYREDUCEDZONEII);
             else if (decision == 4)
-                addTicketToShoppingList(Ticket.SIXTYZONEII);
+                shoppingCart.addTicketToCart(Ticket.SIXTYZONEII);
             else if (decision == 5)
-                addTicketToShoppingList(Ticket.NINETYREDUCEDZONEII);
+                shoppingCart.addTicketToCart(Ticket.NINETYREDUCEDZONEII);
             else if (decision == 6)
-                addTicketToShoppingList(Ticket.NINETYZONEII);
+                shoppingCart.addTicketToCart(Ticket.NINETYZONEII);
         }
     }
-    private int pickQuantity(Scanner keyboard){
+
+    private int pickQuantity(Scanner keyboard) {
         int quantity;
         quantity = UserInputProvider.getInputFromUser(keyboard, "Wybierz ilość biletów (1-9)", 1, 9);
         //disables choosing more tickets than the machine is able to print
-        if (ticketsBought.size() + quantity > printer.getPaperPieces()) {
+        if (shoppingCart.getTicketsQuantity() + quantity > printer.getPaperPieces()) {
             System.err.println("Nie można wydrukować tylu biletów. Spróbuj mniejszą liczbę");
             pickQuantity(keyboard);
         }
         return quantity;
-    }
-    private void addTicketToShoppingList(Ticket ticket){
-        ticketsBought.add(ticket);
-    }
-    public double ticketShoppingListValue(){
-        double sum = 0;
-        for (int i = 0; i < ticketsBought.size(); i++){
-            sum += ticketsBought.get(i).getPrice();
-        }
-        return sum;
-    }
-    public ArrayList<Ticket> getTicketsBought() {
-        return ticketsBought;
     }
 }
