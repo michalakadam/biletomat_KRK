89cf7b64017c8b74049b00ca3b81afec7e480252
Adam <michalak.adam@protonmail.com>
2018-10-08 11:29:10 +0200

    feat: move method for adding coins to change storage

--- src/main/java/com/michalak/adam/app/Bank.java
+++ src/main/java/com/michalak/adam/app/Bank.java
@@ -8,33 +8,17 @@ import java.util.ArrayList;
  * Bank is an object that stores surplus coins from change storage. When change storage capacity for a given
  * coin is achieved additional coins provided by the customer are moved from temporary money storage to
  * the bank. Coins from bank are taken only by authorised employee. They cannot automatically go to
- * change storage when there is not enough coins there.
- * Bank has one field: ArrayList of type Integer. Each index represents the amount of coins of a given type
- * in the bank. Each slot has place for hundred coins.
+ * change storage when there is not enough coins there. You can imagine Bank as unordered sack of coins.
+ *
  */
 public class Bank {
-    ArrayList<Integer> coinsInBank;
+    ArrayList<Coin> coins;
 
-    public Bank(){
-        coinsInBank = new ArrayList<Integer>(6); //there are six types of coins
+    public Bank() {
+        coins = new ArrayList<Coin>();
     }
-    public void addCoin(Coin coin){
-        if(coin.equals(Coin.FIVE) && checkPlaceForAnotherCoin(0))
-            coinsInBank.set(0, coinsInBank.get(0) + 1);
-        else if(coin.equals(Coin.TWO) && checkPlaceForAnotherCoin(1))
-            coinsInBank.set(1, coinsInBank.get(1) + 1);
-        else if(coin.equals(Coin.ONE) && checkPlaceForAnotherCoin(2))
-            coinsInBank.set(2, coinsInBank.get(2) + 1);
-        else if(coin.equals(Coin.POINTFIFTY) && checkPlaceForAnotherCoin(3))
-            coinsInBank.set(3, coinsInBank.get(3) + 1);
-        else if(coin.equals(Coin.POINTTWENTY) && checkPlaceForAnotherCoin(4))
-            coinsInBank.set(4, coinsInBank.get(4) + 1);
-        else if(coin.equals(Coin.POINTTEN) && checkPlaceForAnotherCoin(5))
-            coinsInBank.set(5, coinsInBank.get(5) + 1);
-    }
-    public boolean checkPlaceForAnotherCoin(int index){
-        if(coinsInBank.get(index) < 100)
-            return true;
-        return false;
+
+    public void add(Coin coin) {
+        coins.add(coin);
     }
 }
--- src/main/java/com/michalak/adam/app/ChangeStorage.java
+++ src/main/java/com/michalak/adam/app/ChangeStorage.java
@@ -1,17 +1,48 @@
 package com.michalak.adam.app;
 
-import java.util.ArrayList;
+import com.michalak.adam.helpers.Coin;
 
-/** Change storage is an object that stores coins that are available for giving change to the customer.
- *  Each slot has place for hundred coins. Amount of coins when change storage object is initialized is
- *  picked randomly.
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ *  Change storage is an object that stores coins that are available for giving change to the customer.
+ *  One of change storage fields is ArrayList of type Integer. Each index represents the amount of coins of a given type.
+ *  Amount of coins when change storage object is initialized is picked randomly. Each slot (index of the ArrayList)
+ *  has place for hundred coins.
  */
 
-public class ChangeStorage extends Bank {
-    ArrayList<Integer> coinsInChangeStorage;
-
+public class ChangeStorage {
+    ArrayList<Integer> coins;
+    Bank bank;
     public ChangeStorage(){
-        super();
+        bank = new Bank();
+        coins = new ArrayList<Integer>(6); //there are six types of coins
+        Random randomGenerator = new Random();
+        for(int i = 0; i < coins.size(); i++){
+            coins.set(i, randomGenerator.nextInt(101));
+        }
+    }
 
+    public void addCoin(Coin coin){
+        if(coin.equals(Coin.FIVE) && checkPlaceForAnotherCoin(0))
+            coins.set(0, coins.get(0) + 1);
+        else if(coin.equals(Coin.TWO) && checkPlaceForAnotherCoin(1))
+            coins.set(1, coins.get(1) + 1);
+        else if(coin.equals(Coin.ONE) && checkPlaceForAnotherCoin(2))
+            coins.set(2, coins.get(2) + 1);
+        else if(coin.equals(Coin.POINTFIFTY) && checkPlaceForAnotherCoin(3))
+            coins.set(3, coins.get(3) + 1);
+        else if(coin.equals(Coin.POINTTWENTY) && checkPlaceForAnotherCoin(4))
+            coins.set(4, coins.get(4) + 1);
+        else if(coin.equals(Coin.POINTTEN) && checkPlaceForAnotherCoin(5))
+            coins.set(5, coins.get(5) + 1);
+        else
+            bank.add(coin);
+    }
+    public boolean checkPlaceForAnotherCoin(int index){
+        if(coins.get(index) < 100)
+            return true;
+        return false;
     }
 }
