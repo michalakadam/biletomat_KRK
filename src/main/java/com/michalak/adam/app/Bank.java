8c7e453b7a92341f4aa01fd6b9c2b6a13e9ae50d
Adam <michalak.adam@protonmail.com>
2018-10-08 08:38:03 +0200

    feat: add method to check if another coin can fit in the bank
    If not then the coin will be returned to the customer and they will be
    prompted to pay with another coin.

--- src/main/java/com/michalak/adam/app/Bank.java
+++ src/main/java/com/michalak/adam/app/Bank.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
  * the bank. Coins from bank are taken only by authorised employee. They cannot automatically go to
  * change storage when there is not enough coins there.
  * Bank has one field: ArrayList of type Integer. Each index represents the amount of coins of a given type
- * in the bank.
+ * in the bank. Each slot has place for hundred coins.
  */
 public class Bank {
     ArrayList<Integer> coinsInBank;
@@ -19,17 +19,22 @@ public class Bank {
         coinsInBank = new ArrayList<Integer>(6); //there are six types of coins
     }
     public void addCoin(Coin coin){
-        if(coin.equals(Coin.FIVE))
+        if(coin.equals(Coin.FIVE) && checkPlaceForAnotherCoin(0))
             coinsInBank.set(0, coinsInBank.get(0) + 1);
-        else if(coin.equals(Coin.TWO))
+        else if(coin.equals(Coin.TWO) && checkPlaceForAnotherCoin(1))
             coinsInBank.set(1, coinsInBank.get(1) + 1);
-        else if(coin.equals(Coin.ONE))
+        else if(coin.equals(Coin.ONE) && checkPlaceForAnotherCoin(2))
             coinsInBank.set(2, coinsInBank.get(2) + 1);
-        else if(coin.equals(Coin.POINTFIFTY))
+        else if(coin.equals(Coin.POINTFIFTY) && checkPlaceForAnotherCoin(3))
             coinsInBank.set(3, coinsInBank.get(3) + 1);
-        else if(coin.equals(Coin.POINTTWENTY))
+        else if(coin.equals(Coin.POINTTWENTY) && checkPlaceForAnotherCoin(4))
             coinsInBank.set(4, coinsInBank.get(4) + 1);
-        else if(coin.equals(Coin.POINTTEN))
+        else if(coin.equals(Coin.POINTTEN) && checkPlaceForAnotherCoin(5))
             coinsInBank.set(5, coinsInBank.get(5) + 1);
+    }
+    public boolean checkPlaceForAnotherCoin(int index){
+        if(coinsInBank.get(index) < 100)
+            return true;
+        return false;
     }
 }
