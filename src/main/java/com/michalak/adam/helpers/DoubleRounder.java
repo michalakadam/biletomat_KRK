90cca1a03445e50be2ee38a3e3ec835bdb8840f6
Adam <michalak.adam@protonmail.com>
2018-10-08 07:56:05 +0200

    docs: add javadoc

--- src/main/java/com/michalak/adam/helpers/DoubleRounder.java
+++ src/main/java/com/michalak/adam/helpers/DoubleRounder.java
@@ -3,6 +3,10 @@ package com.michalak.adam.helpers;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+/**
+ * Helper that rounds double values to specified amount of significant digits.
+ * @author www.baeldung.com
+ */
 public class DoubleRounder {
     public static double round(double value, int places) {
         if (places < 0) throw new IllegalArgumentException();
